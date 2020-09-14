fe6f76eda5dc184b331d5f536188317f51ec2e34
Icko92 <denkovski.ivica.92@gmail.com>
2020-09-14 14:57:08 +0200

    Updates

---
+++ precache-manifest.b956c0d738bd6559f2a07709b28dc43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a59f6a7cffde58ce4cc7d4b09deef8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "160cd9bcf16b61b89369",
+    "url": "/monsters-rolodex/static/css/main.487ec004.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160cd9bcf16b61b89369",
+    "url": "/monsters-rolodex/static/js/main.4d136f11.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
