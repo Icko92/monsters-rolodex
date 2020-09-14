6c571a7c133f1622912392f12b5537c85327e846
Icko92 <denkovski.ivica.92@gmail.com>
2020-09-14 15:16:23 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.0e8cc2ec9510b125993714824ad6bcd7.js"
+  "/monsters-rolodex/precache-manifest.b956c0d738bd6559f2a07709b28dc43e.js"
 );
 
 self.addEventListener('message', (event) => {
